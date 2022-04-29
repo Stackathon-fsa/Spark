5ca8c1a4d540a09b7a6f428ffaccc35e41c36893
Brian Clark <briansclark1@gmail.com>
2022-04-28 23:08:12 -0400

    Basic Redux-Toolkit store Initialized

--- client/redux/user.js
+++ client/redux/user.js
@@ -1,34 +1,75 @@
 import axios from "axios"
+import { createSlice, createAsyncThunk } from "@reduxjs/toolkit"
 
-const GET_USER = "GET_USER"
-
-export const getUser = (user) => {
-  return {
-    type: GET_USER,
-    user,
-  }
-}
-
-export const fetchUser = () => {
-    console.log("ayyyyyyyyy")
-    return async (dispatch) => {
-        try {
-         
+export const fetchUser = createAsyncThunk(
+  "user/fetchUser",
+  async () => {
+    try {
       const { data } = await axios.get(`http://localhost:8080/api/users`)
-      dispatch(getUser(data))
+      console.log('Data From Thunk', data);
+      return data 
     } catch (error) {
       console.error(error)
     }
   }
-}
+)
 
-const initialState = []
+export const userSlice = createSlice({
+  name: "user",
+  initialState: {
+    user: [],
+    status: null,
+    error: null,
+  },
+  reducers: {},
+  extraReducers: {
+    [fetchUser.pending]: (state, action) => {
+      state.status = "loading"
+    },
+    [fetchUser.fulfilled]: (state, { payload }) => {
+      // console.log("action is", action)
+      state.user = payload
+      state.status = "success"
+    },
+    [fetchUser.rejected]: (state, action) => {
+      state.status = "failed",
+      state.error = action.payload
+    },
+  },
+})
 
-export default function userReducer(state = initialState, action) {
-  switch (action.type) {
-    case GET_USER:
-      return [action.user]
-    default:
-      return state
-  }
-}
+export default userSlice.reducer;
+
+// const GET_USER = "GET_USER"
+
+// export const getUser = (user) => {
+//   return {
+//     type: GET_USER,
+//     user,
+//   }
+// }
+
+// export const fetchUser = () => {
+//     console.log("ayyyyyyyyy")
+//     return async (dispatch) => {
+//         try {
+
+//           const { data } = await axios.get(`http://localhost:8080/api/users`)
+//           dispatch(getUser(data))
+
+//     } catch (error) {
+//       console.error(error)
+//     }
+//   }
+// }
+
+// const initialState = []
+
+// export default function userReducer(state = initialState, action) {
+//   switch (action.type) {
+//     case GET_USER:
+//       return [action.user]
+//     default:
+//       return state
+//   }
+// }
--- client/store/index.js
+++ client/store/index.js
@@ -2,16 +2,25 @@ import { createStore, combineReducers, applyMiddleware } from "redux"
 import { createLogger } from "redux-logger"
 import thunkMiddleware from "redux-thunk"
 import { composeWithDevTools } from "redux-devtools-extension"
-import userReducer from "../redux/user"
+// import userReducer from "../redux/user"
+import { configureStore } from "@reduxjs/toolkit";
+import userReducer  from "../redux/user"
 
 
-const reducer = combineReducers({
-  user: userReducer,
+
+export default configureStore({
+  reducer: {
+    user: userReducer,
+  },
 })
-const middleware = composeWithDevTools(
-  applyMiddleware(thunkMiddleware, createLogger({ collapsed: true }))
-)
-const store = createStore(reducer, middleware)
 
-export default store
-// export * from "./auth"
+// const reducer = combineReducers({
+//   user: userReducer,
+// })
+// const middleware = composeWithDevTools(
+//   applyMiddleware(thunkMiddleware, createLogger({ collapsed: true }))
+// )
+// const store = createStore(reducer, middleware)
+
+// export default store
+// // export * from "./auth"
