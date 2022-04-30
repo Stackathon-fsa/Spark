8fbb07177b520d0bd9acdd08eadedbb4114b5ecb
Bekim Sinanovic <bekimsina98@gmail.com>
2022-04-30 18:40:16 -0400

    auth works?

--- client/components/Home.js
+++ client/components/Home.js
@@ -1,7 +1,10 @@
 import React from "react"
 import { StyleSheet, Text, View, Button } from "react-native"
+import { useDispatch } from "react-redux"
+import { logout } from "../redux/auth"
 
 export default function Home({ navigation }) {
+  const dispatch = useDispatch();
   return (
     // <View style={styles.container}>
     //   <Text>WELCOME HOME CARLY!</Text>
@@ -12,6 +15,10 @@ export default function Home({ navigation }) {
         title="SingleUser"
         onPress={() => navigation.navigate("SingleUser")}
       />
+      <Button
+        title="Logout"
+        onPress={() => dispatch(logout())}
+      >Logout</Button>
     </View>
   )
 }
